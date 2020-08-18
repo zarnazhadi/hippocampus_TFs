95077aa486df57c127bfc516bccb48361344072e
zxh755 <zxh755@student.bham.ac.uk>
2020-08-18 12:20:41 +0100

    fixed formatting

--- scripts/R/single_tf_all_tissue_types.R
+++ scripts/R/single_tf_all_tissue_types.R
@@ -10,7 +10,8 @@ tf_format <- gsub("_", " ", tf)
 
 # produce scatter plot
 pdf(paste0('/Users/zarnazhadi/Desktop/hippocampus_TFs/expression_data/plots/', tf, "_all_tissue_types_boxplot.pdf"))
-plot <- ggplot(dat, aes(x=tissue_type, y=DDX11L1_expression + 0.0000000001, colour = tissue_type)) + scale_y_log10() +
+plot <- ggplot(dat, aes(x=tissue_type, y=DDX11L1_expression + 0.0000000001, colour = tissue_type)) + 
+  scale_y_log10() +
   geom_boxplot(colour = "red", fill= "orange", alpha = 0.5) + 
   theme( axis.text.x = element_text(angle = 90)) +
   ggtitle(paste0(tf_format, " in all tissue types"))
