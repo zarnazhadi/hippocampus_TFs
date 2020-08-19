dd63017ffc277622d95a7528d96c8abf1556259f
zxh755 <zxh755@student.bham.ac.uk>
2020-08-19 15:42:31 +0100

    ordered and sorted

--- scripts/R/tf_median.R
+++ scripts/R/tf_median.R
@@ -6,22 +6,30 @@ library(tidyverse)
 setwd('/Users/zarnazhadi/Desktop/hippocampus_TFs/expression_data/')
 dat <- fread(file = '10_TFs_expression_data.csv', header = TRUE, sep=',')
 
-tissue_types <- unique(dat$tissue_type)
-median_list <-  list()
-cols <- list()
+#tissue_types <- unique(dat$tissue_type)
+#median_list <-  list()
+#cols <- list()
 
-for (x in 1:length(tissue_types)){
-  tissue <- dat[dat$tissue_type == tissue_types[x]]
-  median <- apply(tissue[,2:11], 2, median)
+#for (x in 1:length(tissue_types)){
+ # tissue <- dat[dat$tissue_type == tissue_types[x]]
+ # median <- apply(tissue[,2:11], 2, median)
 
-  cols[x] <- tissue_types[x]
-  median_list[[x]] <- median
-}
+ 
+#  cols[x] <- tissue_types[x]
+#  median_list[[x]] <- median
+#}
 
 dat <- dat[,-1]
 
 medians <- as.data.frame(dat %>% group_by(tissue_type) %>% summarise_all(funs(median)))
 colnames <- colnames(medians)
 
-apply(medians, 1, order)
+medians <- medians[,-1]
+for(row in 1:nrow(medians[,-1])){
+  sort <-sort(medians[row,])
+  print(medians[row,])
+  print(order)
+}
 
+numbers <- c(0, 1.742,0, 0.01953,0,0.0467,0.06646,0.07319,0.01911,0.01566)
+numbers <- c(1,2,3,4,6,5)
